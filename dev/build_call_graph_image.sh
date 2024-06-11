b99e6896331e23115b9fcccf5c95300ae2ef2d85
crjfisher <CRJFisher@users.noreply.github.com>
2024-06-11 14:37:45 +0100

    add launch configuration for call-stack-detector docker build/push

--- dev/build_call_graph_image.sh
+++ dev/build_call_graph_image.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd ../
-
 docker build -t crjfisher/codecharter-detectcallgraphs -f docker/call-graph-detector/Dockerfile.distroless .
 
 docker push crjfisher/codecharter-detectcallgraphs
