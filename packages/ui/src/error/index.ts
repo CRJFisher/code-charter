bcaa7781ae80061bb6699242accc304b3f2f4c5d
crjfisher <CRJFisher@users.noreply.github.com>
2026-06-28 12:49:25 +0100

    chore(hygiene): remove dead error/index.ts barrel (zero importers)

--- packages/ui/src/error/index.ts
+++
@@ -1,5 +0,0 @@
-export { ErrorLogger, error_logger } from './error_logger';
-export { with_retry, ErrorRecovery, type RetryOptions } from './retry';
-export { ErrorNotificationManager, error_notification_manager, type ErrorNotification } from './error_notification_manager';
-export { ErrorBoundary, DefaultErrorFallback, type ErrorBoundaryProps, type ErrorBoundaryState } from './error_boundary';
-export { ErrorNotifications, use_error_notification, type ErrorNotificationsProps } from './error_notifications';
