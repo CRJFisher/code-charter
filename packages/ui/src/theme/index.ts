531026112c93fc6585a28eab5183c8b7010d3307
crjfisher <CRJFisher@users.noreply.github.com>
2026-06-28 19:30:41 +0100

    chore(hygiene): trim dead re-exports from theme barrel

--- packages/ui/src/theme/index.ts
+++ packages/ui/src/theme/index.ts
@@ -1,5 +1,2 @@
-export { ThemeProviderComponent, use_theme } from './theme_context';
+export { ThemeProviderComponent } from './theme_context';
 export { ThemeSwitcher } from './theme_switcher';
-export { VSCodeThemeProvider } from './vscode_theme_provider';
-export { StandaloneThemeProvider } from './standalone_theme_provider';
-export { default_themes, dark_theme, light_theme } from './default_themes';
